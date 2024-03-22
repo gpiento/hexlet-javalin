45d4d36d2d583b3f36ef692158915de6b3b8d19a
gpiento <zzzest@gmail.com>
2024-03-23 01:07:21 +0300

    add controller /hello

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,16 +2,25 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 
+import java.util.Optional;
+
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
-        app.start(7070); // Стартуем веб-сервер
+        app.get("/hello", ctx -> {
+            Optional<String> name = Optional.ofNullable(ctx.queryParam("name"));
+            if (name.isPresent()) {
+                ctx.result(String.format("Hello, %s!\n", name.get()));
+            } else {
+                ctx.result("Hello, World!\n");
+            }
+        });
+        app.start(7070);
     }
 }
