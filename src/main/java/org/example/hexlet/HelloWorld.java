d937867b140046316443874011caeb3687742224
gpiento <zzzest@gmail.com>
2024-03-22 22:40:38 +0300

    add controller

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,8 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
         app.start(7070); // Стартуем веб-сервер
     }
 }
